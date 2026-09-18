238a44fccdbad79dbb3ce4908ae01d34c83354c5
Thomas Dignoire <tdignoire@pm.me>
2026-09-18 17:36:18 +0200

    Setup Nuxt testing suite dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,8 @@ export default defineNuxtConfig({
     '@nuxt/eslint',
     '@nuxt/ui',
     '@pinia/nuxt',
-    'nuxt-mongoose'
+    'nuxt-mongoose',
+    '@nuxt/test-utils/module',
   ],
 
   devtools: {
